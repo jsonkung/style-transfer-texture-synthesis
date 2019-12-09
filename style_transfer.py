65d8128c7eb6b1cbe8a4285f421403b6c42bcb7a
Daniel Sun <dxsun1998@gmail.com>
2019-12-09 00:00:30 -0500

    branch with combined implementations

--- style_transfer.py
+++ style_transfer.py
@@ -12,7 +12,73 @@ from fuse_content import fuse_content
 from skimage.restoration import denoise_tv_chambolle
 
 
-def style_transfer(content, style, num_pyramid_layers=3, patch_sizes=(33, 21, 13, 9), patch_spacings=(28, 18, 8, 5), num_iters=3, num_irls_iters=10, max_pixel_val=255):
+
+
+####### - begin github additions
+
+from pca import pca, project
+from sklearn.feature_extraction.image import extract_patches
+from sklearn.neighbors import NearestNeighbors
+
+LMAX = 3
+IM_SIZE = 400
+PATCH_SIZES = np.array([33, 21, 13, 9])
+SAMPLING_GAPS = np.array([28, 18, 8, 5])
+IALG = 10
+IRLS_it = 3
+IRLS_r = 0.8
+PADDING_MODE = 'edge'
+# from github: implemented color transfer with histogram matching using interpolation
+def color_transfer(content, style):
+    transfered = np.copy(content)
+    # for each channel of the content, match the cum_histogram with the style's one
+    for i in range(0, content.shape[2]):
+        content_channel = content[:, :, i].flatten()
+        style_channel = style[:, :, i].flatten()
+        # calculate histogram for both content and style
+        content_values, content_indices, content_counts = np.unique(content_channel, return_inverse=True, return_counts=True)
+        style_values, style_counts = np.unique(style_channel, return_counts=True)
+        # calculate cummulative histogram
+        content_cumhist = np.cumsum(content_counts)
+        style_cumhist = np.cumsum(style_counts)
+        # normalize it
+        content_cumhist = content_cumhist / np.max(content_cumhist)
+        style_cumhist = style_cumhist / np.max(style_cumhist)
+        # match using interpolation
+        matched = np.interp(content_cumhist, style_cumhist, style_values)
+        transfered[:, :, i] = matched[content_indices].reshape(content[:, :, i].shape)
+    return transfered
+
+def solve_irls(X, X_patches_raw, patch_size, sampling_gap, style_patches, neighbors, projection_matrix):
+    # current_size = X.shape[0]
+    # Extracting Patches
+    X_patches = X_patches_raw.reshape(-1, patch_size * patch_size * 3)
+    npatches = X_patches.shape[0]
+    if patch_size <= 21:
+        X_patches = project(X_patches, projection_matrix)  # Projecting X to same dimention as style patches
+    # Computing Nearest Neighbors
+    distances, indices = neighbors.kneighbors(X_patches)
+    distances += 0.0001
+    # Computing Weights
+    weights = np.power(distances, IRLS_r - 2)
+    # Patch Accumulation
+    R = np.zeros((X.shape[0], X.shape[1], 3), dtype=np.float32)
+    Rp = extract_patches(R, patch_shape=(patch_size, patch_size, 3), extraction_step=sampling_gap)
+    print("Rp shape:", Rp.shape)
+    X[:] = 0
+    t = 0
+    for t1 in range(X_patches_raw.shape[0]):
+        for t2 in range(X_patches_raw.shape[1]):
+            nearest_neighbor = style_patches[indices[t, 0]]
+            X_patches_raw[t1, t2, 0, :, :, :] += nearest_neighbor * weights[t]
+            Rp[t1, t2, 0, :, :, :] += 1 * weights[t]
+            t = t + 1
+    R += 0.0001  # to avoid dividing by zero.
+    X /= R
+
+######## - end github additions
+
+def style_transfer(content, style, num_pyramid_layers=3, patch_sizes=(33, 21, 13, 9, 5), patch_spacings=(28, 18, 8, 5, 3), num_iters=3, num_irls_iters=10, max_pixel_val=255):
     """
     Perform style transfer via texture synthesis with the following steps
     1. Start at lowest resolution layer of pyramid
@@ -25,10 +91,12 @@ def style_transfer(content, style, num_pyramid_layers=3, patch_sizes=(33, 21, 13
     8. Upscale the image for next pyramid level and repeat 2-8
     """
     # Transfer color and get segmentation mask in 3 channels
-    colored_content = color_transfer(style, content)
+    colored_content = color_transfer(content, style)
+    print("colored_content shape:", colored_content.shape)
+
     segmentation_mask = get_segmentation_mask(content, max_pixel_val=max_pixel_val)
     segmentation_mask = np.dstack([segmentation_mask] * content.shape[-1])
-
+    print("segmentation_mask shape:", segmentation_mask.shape)
     # Get segmentation mask and pyramids
     style_pyramid = get_pyramid(style, num_pyramid_layers)
     content_pyramid = get_pyramid(colored_content, num_pyramid_layers)
@@ -46,16 +114,38 @@ def style_transfer(content, style, num_pyramid_layers=3, patch_sizes=(33, 21, 13
         scaled_segmentation = segmentation_pyramid[L]
 
         for patch_size, patch_spacing in zip(patch_sizes, patch_spacings):
-            style_patches, pca, nn = pca_nn.get_patches_pca_nn(scaled_style, patch_size, patch_spacing=patch_spacing)
+            # style_patches, pca, nn = pca_nn.get_patches_pca_nn(scaled_style, patch_size, patch_spacing=patch_spacing)
+            # print("style_patches shape:", style_patches.shape)
+            # print("nn shape:", nn.shape)
+
+            # from github: new patch matching and robust aggregation
+            ############ - begin github additions
+            style_patches = extract_patches(scaled_style, patch_shape=(patch_size, patch_size, 3), extraction_step=patch_spacing)
+            style_patches = style_patches.reshape(-1, patch_size * patch_size * 3)
+            njobs = 1
+            if (L == 0) or (L == 1 and patch_size <= 13):
+                njobs = -1
+            projection_matrix = 0
+            if patch_size <= 21:
+                new_style_patches, projection_matrix = pca(style_patches)
+                neighbors = NearestNeighbors(n_neighbors=1, p=2, n_jobs=njobs).fit(new_style_patches)
+            else:
+                neighbors = NearestNeighbors(n_neighbors=1, p=2, n_jobs=njobs).fit(style_patches)
+            
+            style_patches = style_patches.reshape((-1, patch_size, patch_size, 3))
 
             for _ in range(num_iters):
-                matches = pca_nn.get_patch_matches(output_image, scaled_style, patch_size, patch_spacing=patch_spacing, pca=pca, nn=nn, reference_patches=style_patches)
-                output_image = aggregate_patches(output_image, matches, patch_spacing, num_irls_iters=num_irls_iters)
-                output_image = fuse_content(output_image, scaled_content, scaled_segmentation)
-                output_image = color_transfer(scaled_style, output_image)
-                cv2.imwrite(f"fusedL{L}P{patch_size}.jpg", output_image)
+                X_patches_raw = extract_patches(output_image, patch_shape=(patch_size, patch_size, 3), extraction_step=patch_spacing)
+                print("X_patches_raw shape:", X_patches_raw.shape)
+                # raise Exception
+                for i in range(IRLS_it):
+                    solve_irls(output_image, X_patches_raw, patch_size, patch_spacing, style_patches, neighbors, projection_matrix)
+            ########## - end github additions
+                # matches = pca_nn.get_patch_matches(output_image, scaled_style, patch_size, patch_spacing=patch_spacing, pca=pca, nn=nn, reference_patches=style_patches)
+                # output_image = aggregate_patches(output_image, matches, patch_spacing, num_irls_iters=num_irls_iters)
+                output_image = fuse_content(output_image, scaled_content, scaled_segmentation)    
+                output_image = color_transfer(output_image, scaled_style)
                 output_image = (denoise_tv_chambolle(output_image, weight=0.05) * max_pixel_val)
-            cv2.imwrite(f"output_L{L}P{patch_size}.jpg", output_image)
         
         # Upscale to match the size of the next layer content image unless last layer
         if L < num_pyramid_layers - 1:
